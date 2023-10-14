a6945724593c3a9f66e6d61a1fb38c062de7e0cc
Yadwinder Singh <yadwindersingh0705@gmail.com>
2023-10-14 22:14:59 +0530

    ADD: category option added sorting category option in dashboard

--- client/src/components/home/HomeDashboard.js
+++ client/src/components/home/HomeDashboard.js
@@ -1,12 +1,13 @@
 import React from 'react'
 import Sidebar from '../sidePages/sideBar/SideBar'
+import SortCategory from './optionInput/SortCategory'
 
 export default function HomeDashboard() {
   return (
     <>
-      <div className='flex h-full'>
+      <div className='flex'>
         <Sidebar />
-        <div>HomeDashboard</div>
+        <SortCategory />
       </div>
     </>
   )
---
+++ client/src/components/home/optionInput/SortOption.js
@@ -0,0 +1,24 @@
+import { AiOutlineBgColors } from "react-icons/ai";
+import { FaAirFreshener } from "react-icons/fa";
+import { SiGooglebigquery } from "react-icons/si";
+
+export const SortOptions = [
+    {
+        name: "Size",
+        desc: "Sort by size of the fruit",
+        value: "size",
+        button: SiGooglebigquery
+    },
+    {
+        name: "Color",
+        desc: "Sort by color of the fruit",
+        value: "color",
+        button: AiOutlineBgColors
+    },
+    {
+        name: "Freshness",
+        desc: "Sort by freshness of the fruit",
+        value: "freshness",
+        button: FaAirFreshener
+    }
+]
