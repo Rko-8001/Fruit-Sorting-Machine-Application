74890a909d9ab210acd0fdc445c383e00039ac7d
Yadwinder Singh <yadwindersingh0705@gmail.com>
2023-10-15 01:50:57 +0530

    ADD: disclaimer
    finished input phase for before sorting process.

--- client/src/components/home/HomeDashboard.js
+++ client/src/components/home/HomeDashboard.js
@@ -1,13 +1,14 @@
 import React from 'react'
 import Sidebar from '../sidePages/sideBar/SideBar'
 import SortCategory from './optionInput/sort/SortCategory'
+import Disclaimer from './optionInput/other/Disclaimer'
 
 export default function HomeDashboard() {
   return (
+    
     <>
       <div className='flex'>
         <Sidebar />
-        <SortCategory />
       </div>
     </>
   )
