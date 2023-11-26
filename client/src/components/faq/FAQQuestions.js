fb1f63ed9eae32dd380f9a69dfcd2a815cef5248
Rko-8001 <yadwindersingh0705@gmail.com>
2023-11-26 19:25:53 +0530

    EDIT: faq section done
    create 4 categories and question for each.

--- client/src/components/faq/FAQQuestions.js
+++ client/src/components/faq/FAQQuestions.js
@@ -1,10 +1,90 @@
-export const Questions = [
-    {
-        question: "How to use the machine?",
-        answer: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni, eum quae. Harum officiis reprehenderit ex quia ducimus minima id provident molestias optio nam vel, quidem iure voluptatem, repellat et ipsa."
-    },
-    {
-        question: "Where to check the stats?",
-        answer: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni, eum quae. Harum officiis reprehenderit ex quia ducimus minima id provident molestias optio nam vel, quidem iure voluptatem, repellat et ipsa."
-    }
-]
+export const Questions = {
+    "General": [
+        {
+            "question": "What is the purpose of the fruit sorter machine?",
+            "answer": "The fruit sorter machine is designed to automate the sorting of fruits based on their size, color, and freshness, utilizing image processing technology with the Jetson Nano."
+        },
+        {
+            "question": "Which types of fruits can the machine sort?",
+            "answer": "The machine is versatile and can sort various fruits based on programmed criteria. It can handle a wide range of fruits, including but not limited to apples, oranges, and peaches."
+        },
+        {
+            "question": "Is the fruit sorter machine suitable for commercial use?",
+            "answer": "Yes, the machine is designed for both home and commercial use. It can efficiently handle large quantities of fruits and streamline the sorting process."
+        },
+        {
+            "question": "How does the machine handle different shapes of fruits?",
+            "answer": "The machine focuses on size, color, and freshness attributes rather than shape, making it adaptable to various fruit shapes within the specified criteria."
+        },
+        {
+            "question": "Can I customize the sorting criteria for specific fruits?",
+            "answer": "Yes, the machine is programmable, and you can customize sorting criteria such as size, color, and freshness based on the specific requirements of different fruits."
+        }
+    ],
+    "Cleaning": [
+        {
+            "question": "How do I clean the machine after use?",
+            "answer": "To clean the machine, power it off and unplug it. Wipe down the exterior with a damp cloth. For the conveyor and sorting areas, follow the provided cleaning instructions in the user manual."
+        },
+        {
+            "question": "Can I wash the conveyor belt and sorting components with water?",
+            "answer": "No, avoid direct water contact with electrical components. Use a damp cloth with a mild cleaning solution for the conveyor belt and sorting components."
+        },
+        {
+            "question": "How often should I clean the machine for optimal performance?",
+            "answer": "Regular cleaning is essential. We recommend cleaning the machine after each use to maintain optimal performance and prevent residue buildup."
+        },
+        {
+            "question": "Are any cleaning agents recommended for the machine?",
+            "answer": "Use mild cleaning agents that do not damage the machine's components. Refer to the user manual for recommended cleaning solutions."
+        },
+        {
+            "question": "Can I disassemble the machine for deep cleaning?",
+            "answer": "Certain components can be disassembled for deep cleaning. Refer to the user manual for instructions on disassembly and reassembly."
+        }
+    ],
+    "Usage": [
+        {
+            "question": "How do I start the fruit sorter machine?",
+            "answer": "Plug in the machine, power it on, and follow the startup instructions in the user manual. Ensure that the Jetson Nano is properly connected and configured."
+        },
+        {
+            "question": "Can the machine handle a mix of different fruits at the same time?",
+            "answer": "Yes, the machine can handle a mix of fruits. It will sort them based on the programmed criteria, providing separate output for each category."
+        },
+        {
+            "question": "What happens if a fruit does not meet the sorting criteria?",
+            "answer": "The machine will divert non-compliant fruits to a separate output or reject bin. You can adjust the sorting criteria to meet your specific requirements."
+        },
+        {
+            "question": "Is there a manual override in case I need to interrupt the sorting process?",
+            "answer": "Yes, the machine typically has a manual override feature that allows you to pause or stop the sorting process if needed. Refer to the user manual for instructions."
+        },
+        {
+            "question": "How do I update the sorting criteria for different fruits?",
+            "answer": "Use the provided interface or software to update the sorting criteria. Follow the programming instructions in the user manual for a seamless customization process."
+        }
+    ],
+    "Troubleshooting": [
+        {
+            "question": "The machine is not turning on. What should I check?",
+            "answer": "Ensure that the power supply is connected properly, and the power outlet is functional. Check the fuse and power switch. If the issue persists, refer to the troubleshooting section in the user manual."
+        },
+        {
+            "question": "Some fruits are not being sorted correctly. How can I address this issue?",
+            "answer": "Review and adjust the sorting criteria for the specific fruits experiencing issues. Ensure that the camera and sensors are clean and unobstructed. Calibration may be necessary."
+        },
+        {
+            "question": "The conveyor belt is not moving. What could be the problem?",
+            "answer": "Check for any obstructions in the conveyor belt. Inspect the motor and drive system. If the issue persists, refer to the troubleshooting section in the user manual for further guidance."
+        },
+        {
+            "question": "The machine is producing an error code. What does it mean?",
+            "answer": "Consult the user manual for a list of error codes and their meanings. Follow the provided steps to resolve the issue associated with the specific error code."
+        },
+        {
+            "question": "The sorting process is slower than usual. How can I improve the speed?",
+            "answer": "Check for any bottlenecks in the sorting criteria or conveyor system. Ensure that the Jetson Nano is configured optimally. If needed, contact technical support for assistance in fine-tuning the system for better performance."
+        }
+    ]
+}
--- client/src/components/faq/FAQs.js
+++ client/src/components/faq/FAQs.js
@@ -1,9 +1,12 @@
-import { useState, Fragment } from 'react';
+import { useState, Fragment, useEffect } from 'react';
 import React from 'react'
 import { Questions } from './FAQQuestions'
+import { FaPlus, FaMinus} from "react-icons/fa";
 
 export default function FAQs() {
-    const [isOpen, setIsOpen] = useState(new Array(Questions.length).fill(false));
+    const [section, setSection] = useState("General");
+    const [isOpen, setIsOpen] = useState(new Array(Questions[section].length).fill(false));
+
 
     const toggleQuestion = (index) => {
         const updatedIsOpen = [...isOpen];
@@ -11,48 +14,124 @@ export default function FAQs() {
         setIsOpen(updatedIsOpen);
     };
 
+    const getSection = (section) => {
+        switch (section) {
+            case 1:
+                setSection("General");
+                break;
+            case 2:
+                setSection("Cleaning");
+                break;
+            case 3:
+                setSection("Usage");
+                break;
+            case 4:
+                setSection("Troubleshooting");
+                break;
+            default:
+                setSection("General");
+        }
+    }
+
+    const getSectionId = (section) => {
+        switch (section) {
+            case "General":
+                return 1;
+            case "Cleaning":
+                return 2;
+            case "Usage":
+                return 3;
+            case "Troubleshooting":
+                return 4;
+            default:
+                return 1;
+        }
+    }
+
+    const showAnswer = (index) => {
+        var updatedIsOpen = [...isOpen];
+        updatedIsOpen[index] = !updatedIsOpen[index];
+        setIsOpen(updatedIsOpen);
+    }
+
+    useEffect(() => {
+        setIsOpen(new Array(Questions[section].length).fill(false));
+    }, [section]);
+
     return (
-        <Fragment>  
-            <section className="bg-white dark:bg-gray-900">
-                <div className="container px-6 py-12 mx-auto">
-                    <h1 className="text-3xl sm:text-xl font-semibold text-center text-gray-800 lg:text-4xl ">
-                        Have any Questions?
+        <Fragment>
+            <section className="bg-white bg-gray-900">
+                <div className="container px-6 py-12 ">
+                    <h1 className="text-3xl sm:text-xl items-center font-semibold text-center text-gray-800 lg:text-4xl ">
+                        Have any Questions ?
                     </h1>
 
                     <div className="mt-8 xl:mt-16 lg:flex lg:-mx-12">
+
                         <div className="lg:mx-12">
-                            <h1 className="text-2xl  font-semibold text-gray-800 ">
+                            <h1 className=" items-center text-2xl font-semibold text-gray-800 ">
                                 Table of Content
                             </h1>
 
-                            <div className="mt-4 text-xl md:text-md space-y-8 lg:mt-8">
-                                <button className="block text-blue-500 hover:underline">General         </button>
-                                <button className="block text-gray-500 hover:underline">Cleaning        </button>
-                                <button className="block text-gray-500 hover:underline">Usage           </button>
-                                <button className="block text-gray-500 hover:underline">Troubleshooting </button>
+                            <div className="mt-4 text-xl md:text-md space-y-8 lg:mt-8 text-gray-500">
+                                <button
+                                    onClick={(e) => { e.preventDefault(); getSection(1) }}
+                                    className={`block ${getSectionId(section) === 1 ? 'text-blue-500' : 'text-gray-500'} hover:underline`}
+                                >
+                                    General
+                                </button>
+                                <button
+                                    onClick={(e) => { e.preventDefault(); getSection(2) }}
+                                    className={`block ${getSectionId(section) === 2 ? 'text-blue-500' : 'text-gray-500'} hover:underline`}
+                                >
+                                    Cleaning
+                                </button>
+                                <button
+                                    onClick={(e) => { e.preventDefault(); getSection(3) }}
+                                    className={`block ${getSectionId(section) === 3 ? 'text-blue-500' : 'text-gray-500'} hover:underline`}
+                                >
+                                    Usage
+                                </button>
+                                <button
+                                    onClick={(e) => { e.preventDefault(); getSection(4) }}
+                                    className={`block ${getSectionId(section) === 4 ? 'text-blue-500' : 'text-gray-500'} hover:underline`}
+                                >
+                                    Troubleshooting
+                                </button>
                             </div>
                         </div>
 
                         <div className="flex-1 mt-8 lg:mx-12 lg:mt-0">
-                            <div>
-                                <button className="flex items-center focus:outline-none">
-                                    <svg className="flex-shrink-0 w-6 h-6 text-blue-500" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M20 12H4"></path></svg>
+                            {Questions[section].map((item, index) => (
+                                <div key={index}>
+                                    <div>
+                                        <button 
+                                            onClick={ (e) => {
+                                                e.preventDefault(); 
+                                                showAnswer(index)
+                                            }} 
+                                            className="flex items-center focus:outline-none"
+                                        >
+                                            { isOpen[index] ? 
+                                                <FaMinus className="w-5 h-5 text-blue-500" /> : 
+                                                <FaPlus className="w-5 h-5 text-blue-500" />}
+                                            <h1 className="mx-4 text-xl text-gray-700 ">
+                                                {item.question}
+                                            </h1>
+                                        </button>
 
-                                    <h1 className="mx-4 text-xl text-gray-700 dark:text-white">
-                                        How i can play for my appoinment ?
-                                        </h1>
-                                </button>
-
-                                <div className="flex mt-8 md:mx-10">
-                                    <span className="border border-blue-500"></span>
-
-                                    <p className="max-w-3xl px-4 text-gray-500 dark:text-gray-300">
-                                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni, eum quae. Harum officiis reprehenderit ex quia ducimus minima id provident molestias optio nam vel, quidem iure voluptatem, repellat et ipsa.
-                                    </p>
+                                        { isOpen[index] && 
+                                            <div className="flex mt-8 md:mx-10">
+                                                <span className="border border-blue-500"></span>
+                                                <p className="max-w-3xl px-4 text-gray-500 ">{item.answer}</p>
+                                            </div>
+                                        }
+                                        <hr className="my-8 border-gray-200 " />
+                                    </div>
                                 </div>
-                            </div>
+                            ))}
 
-                            <hr className="my-8 border-gray-200 dark:border-gray-700" />
+
 
                         </div>
                     </div>
