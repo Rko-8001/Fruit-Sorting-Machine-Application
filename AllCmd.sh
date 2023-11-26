0a7f20af9c78e6af6c3f5fa6ad980241c67840e7
Rko-8001 <yadwindersingh0705@gmail.com>
2023-11-26 14:19:23 +0530

    FIX: fix errors and basic backend setup
    backend setup in flask done

--- AllCmd.sh
+++ AllCmd.sh
@@ -1,7 +1,10 @@
 #!/bin/sh
 BASE_DIR=$(pwd)
-Client_DIR="$BASE_DIR/client"
 
+#######       frontend Cmd      #######
+
+Client_DIR="$BASE_DIR/client"
+Server_DIR="$BASE_DIR/server"
 
 # function to copt deb package to Desktop
 copy_deb_package_to_desktop() {
@@ -20,28 +23,48 @@ install_frontend_dependencies() {
     echo "Frontend dependencies installed."
 }
 
+# function to build frontend
+build_frontend() {
+    cd "$Client_DIR"
+    npm run tauri build
+    echo "Frontend built."
+}
+
+#function to run frontend
+run_frontend() {
+    cd "$Client_DIR"
+    npm run tauri dev
+    echo "Frontend running."
+}
+
+
+######          server Cmd       ######
+
+# function to activate virtual environment for the server
+activate_virtual_env() {
+    cd "$BASE_DIR"
+    source server-env/bin/activate
+    echo "Virtual environment activated."
+}
+
 # function to create virtual environment for the server
 creating_virtual_env() {
     cd "$BASE_DIR"
     virtualenv -p python3 server-env
     echo "Virtual environment created."
-
-    # Activate the virtual environment
-    source server-env/bin/activate
-    echo "Virtual environment activated."
 }
 
 # function to install server dependencies
 install_server_dependencies() {
-    cd "$BASE_DIR/server"
+    cd "$Server_DIR"
     pip install -r ./requirements.txt
     echo "Server dependencies installed."
 }
 
-
-
-
-
-
-
-
+# function to run server
+run_server() {
+    activate_virtual_env
+    cd "$Server_DIR"
+    python3 ./server.py
+    echo "Server running."
+}
