944b99886cb85797a633e6a007108e6e282d7b35
Rko-8001 <yadwindersingh0705@gmail.com>
2023-11-26 15:30:11 +0530

    FIX: user guide page

--- client/src/components/howToUse/HowToUse.js
+++ client/src/components/howToUse/HowToUse.js
@@ -5,21 +5,22 @@ export default function HowToUse() {
     <>
       <section class="bg-white ">
         <div class="container px-6 py-10 mx-auto">
-          <h1 class="text-2xl font-semibold text-gray-800 capitalize lg:text-3xl ">
+          <h1 class=" font-semibold text-gray-800 capitalize lg:text-3xl md:text-xl sm:text-lg">
             Learn how to use  <br/> Fruitastic 
           </h1>
 
           <p class="mt-4 text-gray-500 xl:mt-6 ">
-            You are few steps away from using Fruitastic. Follow the steps below to get started.          </p>
+            You are few steps away from using Fruitastic. Follow the steps below to get started.
+          </p>
 
           <div class="grid grid-cols-1 gap-8 mt-8 xl:mt-12 xl:gap-16 md:grid-cols-2 xl:grid-cols-3">
             {Steps.map((step, index) => (
               <div key={index} class="space-y-3">
                 <span class="inline-block p-2 text-blue-500 bg-blue-100 rounded-full ">
-                    <step.button class="w-8 h-8" />
+                    <step.button class="w-10 h-auto md:w-7 sm:w-5" />
                 </span>
 
-                <h1 class="text-xl font-semibold text-gray-700 capitalize ">{step.name}</h1>
+                <h1 class="text-2xl md:text-xl sm:text-lg font-semibold text-gray-700 capitalize ">{step.name}</h1>
                 <p class="text-gray-500 ">{step.description}</p>
               </div>
             ))}
