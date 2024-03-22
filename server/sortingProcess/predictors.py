15cc373678feef85b7421e341fae82ef42d2adac
Rko-8001 <yadwindersingh0705@gmail.com>
2024-03-22 23:06:00 +0530

    analysis setup

--- server/sortingProcess/predictors.py
+++ server/sortingProcess/predictors.py
@@ -1,10 +1,33 @@
+import cv2
+import numpy as np
+from tensorflow.keras.models import load_model
 
+freshnessModel = load_model("models/freshnessModel.h5")
+
+# Function to preprocess the input image
+def preprocess_image(image):
+    # Resize the image to match the input size expected by the model
+    resized_image = cv2.resize(image, (224, 224))  # Resize to (224, 224)
+    # Normalize pixel values to the range [0, 1]
+    normalized_image = resized_image / 255.0
+    # Expand dimensions to match the expected input shape of the model
+    input_image = np.expand_dims(normalized_image, axis=0)
+    return input_image
+
+# Function to make predictions on the image using the loaded model
+def freshnessPredictor(image):
+    # Preprocess the input image
+    preprocessed_image = preprocess_image(image)
+    # Make predictions using the loaded model
+    predictions = model.predict(preprocessed_image)
+    # Interpret the predictions
+    if predictions[0][0] >= 0.50:
+        return "fresh"
+    else:
+        return "rotten"
 
 def colorPredictor(image):
     return "red"
-
-def freshnessPredictor(image):
-    return "fresh"
 
 def sizePredictor(image):
     return "small"
